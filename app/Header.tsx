81744655c16e6d60a89249c47cc855844eb07d40
alstjd0051 <wsc03002@naver.com>
2022-11-08 00:12:14 +0900

    Update:: Styled

--- app/Header.tsx
+++ app/Header.tsx
@@ -1,9 +1,18 @@
+import Link from "next/link";
 import React from "react";
 
 function Header() {
   return (
-    <div className="p-5 bg-blue-500">
-      <p className="font-bold text-white">I am the Header</p>
+    <div className="p-5 bg-blue-500 flex">
+      <Link href={"/"} className="px-2 py-1 bg-white text-blue-500 rounded-lg">
+        Home
+      </Link>
+      <Link
+        href={"/todos"}
+        className=" mx-2 px-2 py-1 bg-white text-red-500 rounded-lg "
+      >
+        Todos
+      </Link>
     </div>
   );
 }
